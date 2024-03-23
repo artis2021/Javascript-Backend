458e73cbd02bf397636285ac97b396fc95ee8fbe
artis2021 <upasnagupta3333@gmail.com>
2024-03-23 13:36:39 +0530

    added user and video models , bcrypt, jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     username: {
@@ -9,9 +11,77 @@ const userSchema = new Schema({
         trim: true,
         index: true
     },
-    
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        require: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String,
+        require: true,
+    },
+    coverImage: {
+        type: String,
+    },
+    watchHistory: {
+        type: Schema.type.ObjectId,
+        ref: "Video"
+    },
+    password: {
+        type: String,
+        require: [true, "Password is required"]
+    },
+    refreshToken: {
+        type: String
+    }
+
     
 
 }, {timestamps: true})
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 
 export const User = mongoose.model("User", userSchema)
